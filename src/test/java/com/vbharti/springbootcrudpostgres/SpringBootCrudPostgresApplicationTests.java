42b1cff80e6440cb02d78ffa202949d25da3f108
vijayab015 <vijayab015@gmail.com>
2022-11-29 19:09:35 +0530

    Initial commit
    Initial commit

---
+++ src/test/java/com/vbharti/springbootcrudpostgres/SpringBootCrudPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbharti.springbootcrudpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
